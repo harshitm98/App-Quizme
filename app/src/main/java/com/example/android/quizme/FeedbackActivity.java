709eb408b2a6b25ee09ca126ebb71a1af4228a49
harshitm98 <harshitmah98@gmail.com>
2018-03-31 10:25:09 +0530

    Completed the app

--- app/src/main/java/com/example/android/quizme/FeedbackActivity.java
+++ app/src/main/java/com/example/android/quizme/FeedbackActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quizme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FeedbackActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        Button submit = (Button)findViewById(R.id.submit_feedback);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FeedbackActivity.this, "Feedback has been submitted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FeedbackActivity.this, "Thanks for taking the feedback", Toast.LENGTH_SHORT).show();
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
